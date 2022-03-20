2d2632b524f5acaeaad723e8b7cdd0622f7927e3
gyulka <roman.gyulizade@gmail.com>
2022-03-20 21:31:07 +0500

    update

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 def_bet = 1.0
-TOKEN = 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTgwNDcyOCwiaWF0IjoxNjQ0NTkwNDI0LCJleHAiOjE2NDU0NTQ0MjR9.GXhlQbdoX5aHZbmzVDyaiUBD46gXk9Z4WJqfKXrZJRg'
+TOKEN = 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAxMzg5MSwiaWF0IjoxNjQ3NzYxODUyLCJleHAiOjE2NDg2MjU4NTJ9.n2iLpl1cHBozD6qqMVwjd-mGDT-LTbIoiXEEwKour7c'
 tg_token = '5007549074:AAEjYzssYbTc1VhSbxPQqrY52H_MQxYskmI'
 goodasly_email='roman.gyulizade@gmail.com'
 # --------служебные--------
 api_url = 'https://api.csgorun.gg/'
+
+bet1=2.25
+bet5=7.25
+bet10=30.0
+bet20=80.0
+withd=130.0
